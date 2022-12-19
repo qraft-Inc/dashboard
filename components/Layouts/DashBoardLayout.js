7879222993f7b88bc29d67f716dbfc3417636a52
HassanBahati <mukisabahati@gmail.com>
2022-12-19 18:49:09 +0300

    chore: add packages

--- components/Layouts/DashBoardLayout.js
+++ components/Layouts/DashBoardLayout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Link from "next/link";
+import SideBar from "../Sidebar";
 
 export default function DashboardLayout({ children, title }) {
   return (
