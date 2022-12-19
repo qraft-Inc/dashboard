7d599e3b9a6e3c8efd633b068740f8643c2d86e5
HassanBahati <mukisabahati@gmail.com>
2022-12-19 18:22:05 +0300

    refactor: set default meta data and navbar items

--- components/Layout.js
+++ components/Layout.js
@@ -6,28 +6,30 @@ export default function Layout({ children, title }) {
   return (
     <>
       <Head>
-        <title>{title ? title + " - Amazona" : "Amazona"}</title>
-        <meta name="description" content="Ecommerce Website" />
+        <title>{title ? title + " - Qraft Academy" : "Qraft Academy"}</title>
+        <meta name="description" content="Qraft Academy Dashboard Website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex min-h-screen flex-col justify-between">
         <header className="">
           <nav className="flex h-12 justify-between shadow-md items-center px-4">
             <Link href="/">
-              <a className="text-lg font-bold">amazona</a>
+              <a className="text-lg font-bold">Qraft Academy</a>
             </Link>
             <div>
               <Link href="/cart">
-                <a className="p-2">Cart</a>
+                <a className="p-2">Apply</a>
               </Link>
               <Link href="login">
-                <a className="p-2">Login</a>
+                <a className="p-2">Hire Now</a>
               </Link>
             </div>
           </nav>
         </header>
         <main className="container m-auto mt-4 px-4">{children}</main>
-        <footer className="flex justify-center items-center h-10 shadow-inner">Copyright &copy; 2022 Amazona</footer>
+        <footer className="flex justify-center items-center h-10 shadow-inner">
+          Copyright &copy; 2022 Qraft Academy
+        </footer>
       </div>
     </>
   );
